e81705d5640a0dff0fe16c68465677038792b99a
shivamj2603 <shivamj2603@gmail.com>
2020-10-15 03:26:38 +0530

    Added:Multiple elements on to the Binary Search Tree and Tested

--- BinarySearchTreeTest.java
+++ BinarySearchTreeTest.java
@@ -3,16 +3,30 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class BinarySearchTreeTest {
-
 	@Test
 	 public void givenThreeIntegers_whenAdded_shouldReturnSizeAsThree() {
-		BinaryNode<Integer> firstNode = new BinaryNode<Integer>(56);
-		BinaryNode<Integer> secondNode = new BinaryNode<Integer>(30);
-		BinaryNode<Integer> thirdNode = new BinaryNode<Integer>(70);
 		BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<Integer>();
-		binarySearchTree.add(firstNode.key);
-		binarySearchTree.add(secondNode.key);
-		binarySearchTree.add(thirdNode.key);
+		binarySearchTree.add(56);
+		binarySearchTree.add(30);
+		binarySearchTree.add(70);
 		assertEquals(3, binarySearchTree.size());
 	}
+	@Test
+	 public void givenThirteenIntegers_whenAdded_shouldReturnSizeAsThirteen() {
+		BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<Integer>();
+		binarySearchTree.add(56);
+		binarySearchTree.add(30);
+		binarySearchTree.add(70);
+		binarySearchTree.add(22);
+		binarySearchTree.add(40);
+		binarySearchTree.add(60);
+		binarySearchTree.add(95);
+		binarySearchTree.add(3);
+		binarySearchTree.add(11);
+		binarySearchTree.add(63);
+		binarySearchTree.add(16);
+		binarySearchTree.add(65);
+		binarySearchTree.add(67);
+		assertEquals(13, binarySearchTree.size());
+	}	
 }
